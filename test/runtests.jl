f9bc7199414a141c0f33d979f79a81f4795ac286
Donghoon Lee <mdhe1248@gmail.com>
2024-01-15 22:30:00 -0500

    Some example code

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using ImageSliceGUI
+using CoordinateTransformations, Rotations, StaticArrays, Images
 using Test
+
 
 @testset "ImageSliceGUI.jl" begin
     # Write your tests here.
+
+  img = rand(100,100,20)
+  tform, tforms, ps = imslice_gui1(img; movingindices = 1:12)
+  ps1 = update_image_pairs(ps)
+  tforms1 = update_tforms(tforms)
+
 end
